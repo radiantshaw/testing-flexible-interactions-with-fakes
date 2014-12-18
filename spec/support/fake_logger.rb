4b909816a78c0c18ea0dc9e90a705ed9e2ff75ec
Gabe Berke-Williams <gabebw@gabebw.com>
2014-12-18 13:59:24 -0500

    Show empty array instead of nil

--- spec/support/fake_logger.rb
+++ spec/support/fake_logger.rb
@@ -1,22 +1,23 @@
 class FakeLogger
   def initialize
-    @messages = {}
+    @messages = {
+      debug: [],
+      error: [],
+      fatal: [],
+    }
   end
 
   attr_reader :messages
 
   def debug(message)
-    @messages[:debug] ||= []
     @messages[:debug] << message
   end
 
   def error(message)
-    @messages[:error] ||= []
     @messages[:error] << message
   end
 
   def fatal(message)
-    @messages[:fatal] ||= []
     @messages[:fatal] << message
   end
 end
